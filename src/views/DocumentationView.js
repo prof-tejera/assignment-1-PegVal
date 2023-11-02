f4a76e0b54cf96aeca37a729a567fa0ccfda9b73
PegVal <107582197+PegVal@users.noreply.github.com>
2023-11-02 21:46:05 +0100

    documentation

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -20,23 +20,121 @@ const Title = styled.div`
  */
 const Documentation = () => {
   return (
-    <Container>
-      <div>
-        <Title>Documentation</Title>
-        <DocumentComponent
-          title="Loading spinner "
-          component={<Loading />}
-          propDocs={[
-            {
-              prop: "size",
-              description: "Changes the size of the loading spinner",
-              type: "string",
-              defaultValue: "medium",
-            },
-          ]}
-        />
-      </div>
-    </Container>
+    <div className="docs">
+      <Container>
+        <div className="docsContent">
+          <DocumentComponent
+            title="Stopwatch"
+            propDocs={[
+              {
+                prop: "duration",
+                description: "changes the duration of the counter",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+              {
+                prop: "init",
+                description: "No user input. From O to init",
+                type: "num",
+                defaultValue: "0",
+              },
+            ]}
+          />
+        </div>
+      </Container>
+
+      <Container>
+        <div className="docsContent">
+          <DocumentComponent
+            title="Countdown"
+            propDocs={[
+              {
+                prop: "duration",
+                description: "No user input. From init to O",
+                type: "num",
+                defaultValue: "0",
+              },
+              {
+                prop: "init",
+                description: "changes the duration of the counter",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+            ]}
+          />
+        </div>
+      </Container>
+
+      <Container>
+        <div className="docsContent">
+          <DocumentComponent
+            title="XY"
+            propDocs={[
+              {
+                prop: "duration",
+                description: "No user input. From init to O",
+                type: "num",
+                defaultValue: "0",
+              },
+              {
+                prop: "init",
+                description: "changes the duration of the counter",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+              {
+                prop: "rehearsal",
+                description: "changes the number of repeat",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+            ]}
+          />
+        </div>
+      </Container>
+
+      <Container>
+        <div className="docsContent">
+          <DocumentComponent
+            title="Tabata"
+            propDocs={[
+              {
+                prop: "duration",
+                description: "countdown. From init to O",
+                type: "num",
+                defaultValue: "0",
+              },
+              {
+                prop: "init",
+                description: "changes the duration of the counter",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+              {
+                prop: "rehearsal",
+                description: "changes the number of repeat",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+              {
+                prop: "pause",
+                description:
+                  "changes the number of seconds for the pause between each duration",
+                type: "string (converted into num)",
+                defaultValue: "None",
+              },
+              {
+                prop: "remaining",
+                description:
+                  "No user input. Defines the total number of seconds to elapse (including duration, pause and repeat).",
+                type: "Num",
+                defaultValue: "None",
+              },
+            ]}
+          />
+        </div>
+      </Container>
+    </div>
   );
 };
 
